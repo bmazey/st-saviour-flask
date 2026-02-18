2825a22bd01e68e9e6593b0f002545a4a51fae9f
brandon <b.mazey@nyu.edu>
2026-02-18 11:18:31 -0500

    example

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+IMG_DIR = './static'
 
 app = Flask(__name__)
 
+# a handler that returns a message on the '/' page!
 @app.route("/")
-def new_dawn_new_day():
+def serve_text():
     return 'new dawn, new day'
 
+# a handler that returns an html file with an image!
+@app.route("/image")
+def serve_image():
+    return(render_template('image.html'))
+
+# start the application
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
